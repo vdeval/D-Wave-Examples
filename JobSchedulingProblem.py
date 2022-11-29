d346ee9f3c0baede865fa509fe1b14121e1b8c06
Vittorio De Val <vittorio.deval@gmail.com>
2022-11-29 11:11:52 +0000

    First entry

---
+++ JobSchedulingProblem.py
@@ -0,0 +1,9 @@
+###################################################################################################
+# Demo program for Job Scheduling Problem
+###################################################################################################
+
+# ------- Import Section -------
+import numpy as np
+from dimod import ConstrainedQuadraticModel
+from dimod import Binary
+from dwave.system import LeapHybridCQMSampler
